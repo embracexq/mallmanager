beeb954f660beffd866fe1f6810edfbb1a3000b8
熊琴 <1910127213@qq.com>
2021-01-26 10:48:23 +0800

    新建登录分支，新建登录组件并配置路由

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+// import Login from '../components/login/login'
+import Login from '@/components/login/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
-      path: '/'
+      name: 'login',
+      path: '/login',
+      component: Login
     }
   ]
 })
