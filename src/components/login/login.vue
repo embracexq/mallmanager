d6abba54c71b8c1a6297e5fc7da6c879f52e9f6e
熊琴 <1910127213@qq.com>
2021-01-26 15:34:17 +0800

    使用await+async简化代码

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -1,15 +1,14 @@
 <template>
   <div class="login-wrap">
-    <el-form class="login-form" label-position="top" label-width="80px"
-    :model="formdata">
-    <h2>用户登录</h2>
+    <el-form class="login-form" label-position="top" label-width="80px" :model="formdata">
+      <h2>用户登录</h2>
       <el-form-item label="用户名">
-        <el-input v-model="formdata.usernaem"></el-input>
+        <el-input v-model="formdata.username"></el-input>
       </el-form-item>
       <el-form-item label="密码">
         <el-input v-model="formdata.password"></el-input>
       </el-form-item>
-      <el-button class="login-btn" type="primary" @click.prevent="handleLogin" >登录</el-button>
+      <el-button class="login-btn" type="primary" @click.prevent="handleLogin">登录</el-button>
     </el-form>
   </div>
 </template>
@@ -19,27 +18,31 @@ export default {
   data () {
     return {
       formdata: {
-        usernaem: '',
+        username: '',
         password: ''
       }
     }
   },
   methods: {
     // 登录请求
-    handleLogin () {
-      this.$http.post('login', this.formdata).then(res => {
-        // console.log(res)
-        const {
-          data,
-          meta: {msg, status}
-        } = res.data
-        if (status === 200) {
-          this.$message.success(msg)
-          this.$router.push({name: 'home'})
-        } else {
-          this.$message.warning(msg)
+    async handleLogin () {
+      const res = await this.$http.post('login', this.formdata)
+      // console.log(res)
+      const {
+        data,
+        meta: {
+          msg,
+          status
         }
-      })
+      } = res.data
+      if (status === 200) {
+        this.$message.success(msg)
+        this.$router.push({
+          name: 'home'
+        })
+      } else {
+        this.$message.warning(msg)
+      }
     }
   }
 }
@@ -53,12 +56,14 @@ export default {
     justify-content: center;
     align-items: center;
   }
+
   .login-wrap .login-form {
     background-color: #fff;
     width: 400px;
     border-radius: 5px;
     padding: 30px;
   }
+
   .login-wrap .login-btn {
     width: 100%;
   }
